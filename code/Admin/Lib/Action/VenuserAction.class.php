45ae4cb18e62d98540ec3147f5bcd7c38c3d48e6
juxiang <chenyongshidiyi@126.com>
2014-07-28 16:24:34 +0800

    up

--- code/Admin/Lib/Action/VenuserAction.class.php
+++ code/Admin/Lib/Action/VenuserAction.class.php
@@ -6,12 +6,17 @@ class VenuserAction extends CommonAction {
 		
 		$con = parent::_search('Venuser');
 		
-		//dump($con);
-		//die;
+		
+		if(!empty($con['account'][1]) )
+			$where=" and t1.account like '".$con['account'][1]."'";
+		
+	
 		
 		$sql="select t1.id,t2.account,t2.nickname,t3.venno,t3.venname  from wx_venuser as t1" 
 					." inner join `wx_user` as t2 on t1.account=t2.account"
-					." inner join `wx_vendor` as t3 on t1.venno=t3.venno";
+					." inner join `wx_vendor` as t3 on t1.venno=t3.venno"
+					." where t3.venno=$_SESSION[login_venno]" .$where;
+		
 		$arr=M("Venuser");
 		$list= $arr->query($sql);
 		//$list = M("Venuser")->where($con)->order("venno")->select();		
@@ -29,56 +34,74 @@ class VenuserAction extends CommonAction {
         $this->gdCat=$array;
         parent::edit();
     } 
-    public function treeLookup() {
-        $gd = M("Organization");
-        
-        $where = array
-        (
-        		venno=>$_SESSION['login_venno'],
-        );
-        
-        $menu = $gd->field('id,pid,title')->where($where)->select();
-        $menu = arrToTree($menu, 0);
-      
-        $this->assign('menu', $menu);
-        $this->display();
-    }
+   
     
-	function _filter(&$map){
-		  $map['id'] = array('egt',2);
-		$map['account'] = array('like',"%".$_POST['account']."%");
-	}
-	    
 	// 插入数据
 	public function insert() {
 		// 创建数据对象
-		$User	 =	 D("User");
+		$User	 =	 D("Venuser");
+		
+		
 		if(!$User->create()) {
 			$this->error($User->getError());
 		}else{
+			
+			
+			
 			// 写入帐号数据
-			if($result	 =	 $User->add()) {
-				$this->addRole($result);
+			$v=$_POST['venno'];
+			$a=$_POST['cat_account'];
+			
+			$sql="insert into `wx_venuser`(`venno`,`account`) values('$v','$a')";
+				
+			if(	 $User->execute($sql)) {
+			
 				$this->success('用户添加成功！');
 			}else{
 				$this->error('用户添加失败！');
 			}
 		}
+		parent::insert();
+	}
+	
+	public function treeLookup() {
+		
+		
+		$gd = M("User");
+	
+		if($_SESSION['loginAccount'] !='admin')
+			$where = array
+			(
+				venno=>$_SESSION['login_venno'],
+			);
+		else 
+			$where = array
+			(
+					venno=>$_SESSION['login_venno'],
+			);
+	
+		
+		$menu = $gd->field('id,account,nickname')->where($where)->select();
+		
+	
+		$this->assign('list', $menu);
+		$this->display();
 	}
 
-    public function treeLookupven() {
-    	$gd = M("Vendor");
+    public function changeaccindex() {
+    	
+    	$con = parent::_search('Venuser');
+    	$gd = M("User");
+    	
+    	if(!empty($con['account'][1]) )
+    		$where=" and account like '".$con['account'][1]."'";
     
-    	$where = array
-    	(
-    			venno=>$_SESSION['login_venno'],
-    	);
-    
-    	$menu = $gd->field('id,pid,title')->where($where)->select();
-    	$menu = arrToTree($menu, 0);
-    
-    	$this->assign('menu', $menu);
-    	$this->display();
+    	$sql="select 'id',`account`,`nickname` from `wx_user` where 1=1 ".$where;
+    	
+    	$menu = $gd->query($sql);
+		
+		$this->assign('list', $menu);
+		$this->display();
     }
     
 }
